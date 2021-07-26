37154f2ffb56e00a177592e0df0ae02d5ebdc034
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-07-26 20:13:33 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "not-grocy-server-asp.net's development flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = pkgs.mkShell {
+            packages = [
+              (with pkgs.dotnetCorePackages; combinePackages [
+                aspnetcore_5_0
+                sdk_5_0
+                # net_5_0
+              ])
+              pkgs.mono
+              pkgs.vscode
+            ];
+          };
+        }
+      );
+}
