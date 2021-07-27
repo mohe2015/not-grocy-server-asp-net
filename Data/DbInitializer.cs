279ccc18eb859a38a73a93b5f28cc21ec70ca6a6
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-07-27 17:46:30 +0200

    Implement database seeding

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,30 @@
+using StockApi.Models;
+using System;
+using System.Linq;
+
+namespace StockApi.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(StockContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any stock items.
+            if (context.StockItems.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var stockItems = new StockItem[]
+            {
+            
+            };
+            foreach (StockItem s in stockItems)
+            {
+                context.StockItems.Add(s);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StockApi.Data;
+using StockApi.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace not_grocy_server_asp_net
 {
@@ -13,7 +16,29 @@ namespace not_grocy_server_asp_net
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<StockContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
