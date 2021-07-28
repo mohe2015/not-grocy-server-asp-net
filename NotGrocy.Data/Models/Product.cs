9065490579401f32add2ead1f8e65bb814c078d4
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-07-29 00:46:47 +0200

    Jo

--- NotGrocy.Data/Models/Product.cs
+++ NotGrocy.Data/Models/Product.cs
@@ -26,16 +26,77 @@ namespace NotGrocy.Models
 
         [Column("product_group_id", TypeName = "INTEGER")]
         public long? ProductGroupId { get; set; }
-        
-        [Column("amount")]
-        public double? Amount { get; set; }
+
+        [Column("active")]
+        public bool Active;
+
+        [Column("location_id")]
+        public long LocationId { get; set; }
+
         [Column("shopping_location_id")]
         public long? ShoppingLocationId { get; set; }
-        [Column("last_price", TypeName = "DECIMAL(15, 2)")]
-        public byte[] LastPrice { get; set; }
-        [Column("row_created_timestamp", TypeName = "DATETIME")]
-        public byte[] RowCreatedTimestamp { get; set; }
-        [Column("note")]
-        public string Note { get; set; }
+
+        [Column("qu_id_purchase", TypeName = "INT")]
+        public long QuIdPurchase { get; set; }
+
+        [Column("qu_id_stock", TypeName = "INT")]
+        public long QuIdStock { get; set; }
+
+        [Column("qu_factor_purchase_to_stock")]
+        public double QuFactorPurchaseToStock { get; set; }
+
+        [Column("min_stock_amount")]
+        public long MinStockAmount { get; set; }
+
+        [Column("default_best_before_days")]
+        public long DefaultBestBeforeDays { get; set; }
+
+        [Column("default_best_before_days_after_open")]
+        public long DefaultBestBeforeDaysAfterOpen { get; set; }
+
+        [Column("default_best_before_days_after_freezing")]
+        public long DefaultBestBeforeDaysAfterFreezing { get; set; }
+
+        [Column("default_best_before_days_after_thawing")]
+        public long DefaultBestBeforeDaysAfterThawing { get; set; }
+
+        [Column("picture_file_name")]
+        public string? PictureFileName { get; set; }
+
+        [Column("enable_tare_weight_handling")]
+        public bool EnableTareWeightHandling;
+
+        [Column("tare_weight")]
+        public double TareWeight { get; set; }
+
+        [Column("not_check_stock_fulfillment_for_recipes")]
+        public bool? NotCheckStockFulfillmentForRecipes;
+
+        [Column("parent_product_id")]
+        public long ParentProductId { get; set; }
+
+        [Column("calories")]
+        public long Calories { get; set; }
+
+        [Column("cumulate_min_stock_amount_of_sub_products")]
+        public bool? CumulateMinStockAmountOfSubProducts;
+
+        [Column("due_type")]
+        public long DueType { get; set; }
+
+        [Column("quick_consume_amount")]
+        public long QuickConsumeAmount { get; set; }
+
+        [Column("hide_on_stock_overview")]
+        public bool? HideOnStockOverview;
+
+        [Column("row_created_timestamp")]
+        public DateTime RowCreatedTimestamp { get; set; }
+
+        [Column("default_print_stock_label")]
+        public long DefaultPrintStockLabel { get; set; }
+
+        [Column("allow_label_per_unit")]
+        public bool AllowLabelPerUnit;
     }
 }
